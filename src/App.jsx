da2897c86adaf1b76657d68351347e455d671d0e
Joao Paulo Dorea <joaopdorea@gmail.com>
2022-10-01 17:14:34 -0300

    feat(calendar): add module with first page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Calendar from './modules/calendar/pages/Calendar.jsx'
+
+function App() {
+  return (
+    <div>
+      Hello world
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
