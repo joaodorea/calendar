bbf4e1ddad2cc20ca07cd3aaae91a7aaeb0d84e2
Joao Paulo Dorea <joaopdorea@gmail.com>
2022-10-01 19:50:36 -0300

    feat(calendar): add simple add reminder with native alert

--- src/modules/calendar/pages/Calendar.jsx
+++ src/modules/calendar/pages/Calendar.jsx
@@ -1,6 +1,7 @@
-// import {useState} from 'react'
+import {useState} from 'react'
 
 function Calendar() {
+  const [reminders, setReminder] = useState([])
   const currentMonth = new Date().getMonth()
   const totalDaysInMonth = new Date(2022, currentMonth + 1, 0).getDate()
   const remainingDays = 6 - new Date(2022, currentMonth, totalDaysInMonth).getDay()
@@ -9,7 +10,11 @@ function Calendar() {
   const diff = 0 - (new Date(2022, currentMonth, 1).getDay()) + 1
   const weeks = []
 
-  const selectDate = (date) => console.log(date)
+  const selectDate = (date) => {
+    const message = prompt('Qual lembre voce quer salvar?')
+    setReminder([...reminders, {date, message}])
+    console.log(reminders)
+  }
 
   for(let i = diff; i <= totalDays; i ++) {
     const currentDate = new Date(2022, currentMonth, i)
@@ -25,12 +30,22 @@ function Calendar() {
   return (
     <div>
       <div>Calendar</div>
-      {weeks.map(w => 
-        <>
-          <span className="date-item" onClick={() => selectDate(w.date)}>{w.day} </span>
-          {w.weekDay === 6 && <br />}
-        </>
-      )}
+      {weeks.map(w => {
+        // improve how dates are compared
+        const messages = reminders.filter(({date}) => +date === +w.date)
+
+        return (
+          <>
+            <span className="date-item" onClick={() => selectDate(w.date)}>
+              {w.day}
+              {messages.length ? messages.map(({message}) => 
+                <span className="date-item-reminder">{message}</span>)
+              : null}
+            </span>
+            {w.weekDay === 6 && <br />}
+          </>
+        )
+      })}
     </div>
   )
 }
