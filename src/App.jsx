8b58f6cd3e63a59aa98ddf8819b2bf8bf2b1115f
Joao Paulo Dorea <joaopdorea@gmail.com>
2022-10-01 19:06:55 -0300

    feat(calendar): create calendar layout based on current month

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Calendar from './modules/calendar/pages/Calendar.jsx'
 function App() {
   return (
     <div>
-      Hello world
       <Calendar />
     </div>
   );
--- src/modules/calendar/pages/Calendar.jsx
+++ src/modules/calendar/pages/Calendar.jsx
@@ -1,6 +1,37 @@
+// import {useState} from 'react'
+
 function Calendar() {
+  const currentMonth = new Date().getMonth()
+  const totalDaysInMonth = new Date(2022, currentMonth + 1, 0).getDate()
+  const remainingDays = 6 - new Date(2022, currentMonth, totalDaysInMonth).getDay()
+  const totalDays = totalDaysInMonth + remainingDays
+
+  const diff = 0 - (new Date(2022, currentMonth, 1).getDay()) + 1
+  const weeks = []
+
+  const selectDate = (date) => console.log(date)
+
+  for(let i = diff; i <= totalDays; i ++) {
+    const currentDate = new Date(2022, currentMonth, i)
+
+    weeks.push({
+      date: currentDate,
+      day: currentDate.getDate(),
+      weekDay: currentDate.getDay(),
+      isCurrentMonth: currentDate.getMonth() === currentMonth,
+    })
+  }
+
   return (
-    <div>Calendar</div>
+    <div>
+      <div>Calendar</div>
+      {weeks.map(w => 
+        <>
+          <span className="date-item" onClick={() => selectDate(w.date)}>{w.day} </span>
+          {w.weekDay === 6 && <br />}
+        </>
+      )}
+    </div>
   )
 }
 
